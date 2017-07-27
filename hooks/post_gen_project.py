ca615b8ad1e0364b760f4e29b9d6e944e4a44e74
hborcher <hborcher@illinois.edu>
2017-07-27 11:56:19 -0500

    remove tox.ini, docs, .travis.yml, deployment.yml, and script data if not using them.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,12 +12,25 @@ def remove_dir(dirpath):
     shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirpath))
 
 if __name__ == "__main__":
-    if '{{ cookiecutter.use_tox }}' != "y":
-        remove_file("tox.ini")
-
     if '{{ cookiecutter.use_pytest }}' != "y":
         remove_dir("tests")
 
+    if '{{ cookiecutter.use_tox }}' != "y":
+        remove_file("tox.ini")
+
+    if '{{ cookiecutter.use_sphinx}}' != "y":
+        remove_dir("docs")
+
+    if '{{ cookiecutter.use_travis_ci}}' != "y":
+        remove_file(".travis")
+
     if '{{ cookiecutter.use_jenkins_pipeline}}' != "y":
         remove_file("Jenkinsfile")
+
+    if '{{ cookiecutter.create_deployment_yml}}' != "y":
+        remove_file("deployment.yml")
+
+    if '{{ cookiecutter.script["add_script"}}' != "y":
+        remove_file("{{ cookiecutter.project_slug }}/__main__.py")
+        remove_file("{{ cookiecutter.project_slug }}/cli.py")
 
