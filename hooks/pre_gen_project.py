28b69307d2948f8c793cac069357d97a13ea211e
hborcher <hborcher@illinois.edu>
2017-07-27 11:56:48 -0500

    validate script information

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,10 @@
 import sys
 
-{%- if cookiecutter.add_script == "y" %}
+{%- if cookiecutter.script["add_script"] == "y" %}
 contains_script = True
-script_name = "{{ cookiecutter.cli_command_name }}"
+script_name = '{{ cookiecutter.script["cli_command_name"] }}'
 if script_name.strip() == "":
     print("Error: Scripts need a valid cli_command_name")
     sys.exit(1)
 {%- endif %}
+
